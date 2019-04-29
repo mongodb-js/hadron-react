2d3ea662eaa9c38b0dcebd1800885bef0d2e5087
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2019-04-29 15:48:06 -0400

    remove console log

--- packages/hadron-react-components/lib/sortable-table.js
+++ packages/hadron-react-components/lib/sortable-table.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
-
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -229,9 +227,7 @@ var SortableTable = function (_React$Component) {
         if (_this3.props.removable) {
           // add a column with a delete button if the `removable` prop was set
           var valueCell = row[_this3.props.valueIndex];
-          console.log('typeof', typeof valueCell === 'undefined' ? 'undefined' : _typeof(valueCell));
           var valueStr = _this3.getStringValueFromCell(valueCell);
-          debugger;
           cells.push(React.createElement(
             'td',
             {
